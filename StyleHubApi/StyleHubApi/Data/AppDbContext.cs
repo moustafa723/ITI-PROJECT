ea9457e8a963c00f1254bfefe458f69b709037c8
nourinawadd <nourinawad@gmail.com>
2025-09-04 23:53:55 +0300

    update appdbcontext for new models

--- StyleHubApi/StyleHubApi/Data/AppDbContext.cs
+++ StyleHubApi/StyleHubApi/Data/AppDbContext.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using StyleHubApi.Models;
-using System;
-
 
 namespace StyleHubApi.Data
 {
@@ -15,19 +13,10 @@ namespace StyleHubApi.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Cart> Carts { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
-
-
-
+        public DbSet<Payment> Payments { get; set; }
     }
-
-
-
-
-
 }
-       
-       
-    
