82fedbb68861f2e2114ff4c133ac4300d1e32e92
mennaelbana <mennaelbana15@gmail.com>
2025-09-04 11:23:11 +0300

    add model Catogery and Product

---
+++ StyleHubApi/StyleHubApi/models/product.cs
@@ -0,0 +1,17 @@
+﻿namespace StyleHubApi.models
+{
+    public class product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Size { get; set; }
+        public string color { get; set; }
+        public string image { get; set; }
+        public int Rating { get; set; }
+        public int Review { get; set; }
+        public string in_stock { get; set; }
+        public ICollection<Catogery> catogeries { get; set; }
+    }
+}
