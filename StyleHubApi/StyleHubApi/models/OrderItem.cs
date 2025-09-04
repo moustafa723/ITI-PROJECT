527f3c94b5bc9b4f6b83bc40e42ce66d9f9bdf04
nourinawadd <nourinawad@gmail.com>
2025-09-04 23:41:32 +0300

    update orderitems model

--- StyleHubApi/StyleHubApi/models/OrderItem.cs
+++ StyleHubApi/StyleHubApi/models/OrderItem.cs
@@ -8,10 +8,10 @@ namespace StyleHubApi.Models
         public int OrderId { get; set; }
         public int ProductId { get; set; }
         public int Quantity { get; set; }
+
         [Precision(18, 2)]
         public decimal Price { get; set; }
 
         public virtual Order Order { get; set; }
-
     }
 }
