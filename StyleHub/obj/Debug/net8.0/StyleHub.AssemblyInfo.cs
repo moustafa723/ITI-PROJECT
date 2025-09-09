f74fd0f72218930cd7f181a08276ae2e0126c8b9
sheroukmenshawy <sheroukelmenshawy335@gmail.com>
2025-09-09 10:14:18 +0300

    Edit (signout button,cart view)

--- StyleHub/obj/Debug/net8.0/StyleHub.AssemblyInfo.cs
+++ StyleHub/obj/Debug/net8.0/StyleHub.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StyleHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204f7910fe4f02be081ad1f4ac119de75f9756af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39d4a9d199b7c20bc832e3f839057188bdaf72ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("StyleHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StyleHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
